4c8778774adaeb438839e3cfd55c6cc5a20858e4
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-05-20 15:03:51 +0200

    display all data of profile

--- Nextep/components/ProfileData.js
+++ Nextep/components/ProfileData.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
 import { IMG_URL } from "@env"
+import Moment from "moment";
 
 import APIKit from "./Api";
 
@@ -10,23 +11,27 @@ export default class DataProfileView extends Component {
     this.state = { profile: "", profileData: [] }
   }
 
-  getProfileData() {
-      //{ id: 26, username: "cyril", firstname: "Cyril", lastname: "Goldenschue", email: "Cyril.Goldenschue@cpnv.ch", picture: "g3.png", created_at: "2022-05-20T06:35:49.000000Z", updated_at: "2022-05-20T06:35:49.000000Z" }
-    APIKit.getProfile().
-      then((res) => {
-        let data = res.data
+    getProfileData() {
+        //{ id: 26, username: "cyril", firstname: "Cyril", lastname: "Goldenschue", email: "Cyril.Goldenschue@cpnv.ch", picture: "g3.png", created_at: "2022-05-20T06:35:49.000000Z", updated_at: "2022-05-20T06:35:49.000000Z" }
+        APIKit.getProfile().
+        then((res) => {
+            let data = res.data
+            Moment.locale("fr");
             const profileShift = (
                 <View>
+                    <Text>{data.username} </Text>
                     <Image style={styles.logo} source={{uri: {IMG_URL}.IMG_URL+data.picture}} />
                     <Text>{data.firstname} {data.lastname}</Text>
+                    <Text>{data.email}</Text>
+                    <Text>Création du compte : {Moment(data.created_at).format("DD MMM Y")}</Text>
                 </View>
             );
             this.setState({
                 profileData: profileShift,
             })
         })
-      }
-  
+    }
+    
 
   
   componentDidMount() {
