2dc239a4d0d404a7976fe1727b72e6430cd0d2b8
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-06-09 09:38:55 +0200

    Centralise of storage method

---
+++ Nextep/components/Storage.js
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class Storage {
+
+    async set(name, data) {
+      await AsyncStorage.setItem(name, data)
+    }
+
+    async get(name){
+      return await AsyncStorage.getItem(name)
+    }
+
+    async remove(name){
+        await AsyncStorage.removeItem(name)
+    }
+    
+}
+
+const StorageKit = new Storage();
+
+export default StorageKit;
