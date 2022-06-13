16ed34cd23b4d95120939002a7531909b39d3da7
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2022-06-13 16:53:45 +0200

    create photo screen

--- Nextep/screens/common/PhotoScreen.js
+++ Nextep/screens/common/PhotoScreen.js
@@ -1,32 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { Camera } from 'expo-camera';
 import StorageKit from "../../components/Storage";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function PhotoScreen(props) {
   const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [capturedImage, setCapturedImage] = useState(null)
+  let camera;
 
   useEffect(() => {
     (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    if(data.substring(0, 2) === "0x"){
-        StorageKit.set("qr_scan", data);
-        props.navigation.reset({
-            index: 0,
-            routes: [{ name: 'EditProfile'}], 
-        })
-      
-    }else{
-        alert("Ceci n'est pas une adresse de compte porte-monnaie")
-    }
-  }   
+  const Pic = async () => {
+    if(!camera) return
+    const photo = await camera.takePictureAsync()
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+
+  const __retakePicture = () => {
+    setCapturedImage(null)
+    setPreviewVisible(false)
+  }
+
+  const __savePhoto = () => {
+
+  }
+
+  const CameraPreview = ({photo, savePhoto, retakePicture}) => {
+    return (
+      <View
+        style={{
+          backgroundColor: 'transparent',
+          flex: 1,
+          width: '100%',
+          height: '100%',
+
+        }}
+      >
+        <ImageBackground
+          source={{uri: photo && photo.uri}}
+          style={{
+            flex: 1,
+            justifyContent: "flex-end"
+          }}
+        >
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginBottom: 10,
+          marginLeft: 10,
+          marginRight: 10,
+        }}>
+        <TouchableOpacity
+          onPress={retakePicture}>
+          <Text style={{color: "#FFFFFF"}}>refaire</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={savePhoto}>
+          <Text style={{color: "#FFFFFF"}}>utiliser</Text>
+        </TouchableOpacity>
+        </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+
+  Back = () => {
+    props.navigation.reset({
+      index: 0,
+      routes: [{ name: 'EditProfile'}], 
+    })
+  }
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -37,11 +88,30 @@ export default function PhotoScreen(props) {
 
   return (
     <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      {previewVisible && capturedImage ? (
+        <CameraPreview photo={capturedImage}  savePhoto={__savePhoto} retakePicture={__retakePicture}/>
+      ) : (
+        <>
+          <Camera
+            style={StyleSheet.absoluteFillObject}
+            ref={(r) => {
+              camera = r
+            }}
+          />
+          <View style={styles.button}>
+          <TouchableOpacity 
+            style={styles.littleButton}
+            onPress={() => Back()}>
+            <Image style={styles.littleButton} source={require("../../assets/back-arrow.png") } />
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.littleButton}
+            onPress={() => Pic()}>
+            <Image style={styles.littleButton} source={require("../../assets/camera.png") } />
+          </TouchableOpacity>
+          </View>
+        </>
+          )}
     </View>
   );
 }
@@ -50,6 +120,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
   },
+  littleButton: {
+    height: 50,
+    width: 58
+  },  
+  button: {
+    flexDirection: "row",
+    marginRight: 10,
+    marginTop: 30,
+    justifyContent: "flex-end"
+  }
 });
