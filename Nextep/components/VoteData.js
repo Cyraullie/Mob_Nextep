6b22f716ac43cec3497b898829ca382fb339b682
Cyraullie <cyril.goldenschue@gmail.com>
2022-07-27 15:20:16 +0200

    Create display vote listing

---
+++ Nextep/components/VoteData.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, Image, Dimensions, ScrollView, Alert } from "react-native";
+import { Card } from "react-native-elements";
+import Moment from "moment";
+import * as SecureStore from 'expo-secure-store';
+import axios from "axios";
+import { IMG_URL, BASE_URL } from "@env"
+import { TextInput, TouchableHighlight } from "react-native-gesture-handler";
+
+export default class DataVoteView extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { voteData: []}
+  }
+
+  getData = () => {
+    SecureStore.getItemAsync("user_token").then(
+      (token) => {
+        this.setState({ userToken: token });
+        const axiosConfig = {headers: { Authorization: "Bearer " + token}};
+        axios.get(BASE_URL + "voting_topics", axiosConfig)
+        .then((response) => {
+          this.getVotingData(response.data) 
+        })
+      .catch(error => {
+        console.log(error);
+      }); 
+    });
+  }
+
+
+  
+  getVotingData(data){
+    const votingArr = data
+    const votingData = []
+
+    for(let i = 0; i < votingArr.length; i++){
+        console.log(votingArr[i])
+        votingData.push(
+        <>
+            <Card style={styles.cardContainer} containerStyle={styles.cardFont}>
+                <Text style={styles.cardTitle}>{votingArr[i].subject}</Text>
+                <Text>{votingArr[i].description}</Text>
+                <View style={styles.votingButton}>
+                    <TouchableHighlight style={[styles.button, styles.check]} >
+                        <Image style={styles.iconButton} source={require("../assets/check.png") } />
+                    </TouchableHighlight>
+                    <TouchableHighlight style={[styles.button, styles.cross]} >
+                        <Image style={styles.iconButton} source={require("../assets/cross.png") } />
+                    </TouchableHighlight>
+                </View>
+            </Card>
+        </>
+        
+        )
+    }
+
+    Moment.locale("fr");
+    
+    const votingShift = (
+        <>
+            {votingData.map(voting_list => (  
+            <>
+                {voting_list}
+            </>
+            ))}
+        </>
+            
+    );
+    this.setState({
+        voteData: votingShift,
+    })
+        
+  }
+  
+  componentDidMount() {
+    this.getData()
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.voteData}
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: "row",
+    marginLeft: 5,
+    marginBottom: 15,
+  },
+  votingButton: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 10
+  },
+
+
+  cardFont: {
+    backgroundColor: "#0693e3",
+    width: Dimensions.get("screen").width - (Dimensions.get("screen").width * .08),
+    height: "auto",
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginRight: "auto",
+    width: "60%"
+  },
+  button: {
+    height:50,
+    width: 50,
+    padding: 10,
+    marginLeft: 10
+  },
+  check: {
+    backgroundColor: "#16cd00",
+  },
+  cross: {
+    backgroundColor: "#cd0000"
+  },
+  iconButton: {
+    width: 30,
+    height: 30,
+  }
+});
+
+
+
+
